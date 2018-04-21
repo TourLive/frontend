cdaf424eae5497275dffde47063220c28016782d
Urs Forrer <mail@ursforrer.ch>
2018-04-21 12:08:27 +0200

    Added elements to settings form
    Added elements to settings form

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Header} from "semantic-ui-react";
+import {Header, Button, Divider, Form} from "semantic-ui-react";
 import {Helmet} from "react-helmet";
 
 class Settings extends Component {
@@ -10,7 +10,20 @@ class Settings extends Component {
                     <title>Einstellungen</title>
                 </Helmet>
                 <Header as="h1" color='red'>Einstellungen</Header>
-                <h1>Einstellungen</h1>
+                <Header as="h3">Benachrichtigungen</Header>
+                <Button className="App-Button" color="green" fluid>Ein</Button><br/>
+                <Button className="App-Button" color="red" fluid >Aus</Button>
+                <Divider />
+                <Header as="h3">Etappe auswählen</Header>
+                <Divider />
+                <Header as="h3">Aktualisierungsintervall</Header>
+                <Form>
+                  <Form.Field control="input" label="Aktualisierungsparameter in Sekunden einstellen"></Form.Field>
+                  <Button primary fluid >Aktualisierungsintervall setzen</Button>
+                </Form>
+                <Divider />
+                <Header as="h3">Copyright</Header>
+              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
             </div>
         );
     }
