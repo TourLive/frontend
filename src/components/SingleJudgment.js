efdaa19eff483f3f375332878f1b6581721de954
Urs Forrer <mail@ursforrer.ch>
2018-04-23 18:25:56 +0200

    Added component for SingleJudgment (its working)
    Added component for SingleJudgment (its working)

---
+++ src/components/SingleJudgment.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {Header} from "semantic-ui-react";
+import {connect} from "react-redux";
+
+class SingleJudgment extends Component {
+  render() {
+    const judgment = this.props.data;
+    const {riders} = this.props;
+    /*let rider = riders.find((e) => {
+      return e.id === trikot.riderId;
+    });
+    console.log(rider);*/
+    console.dir(judgment);
+
+    return(
+      <div>
+        <Header as="h1">Wertung | {judgment.name}</Header>
+        {judgment.reward.points.map((reward,i) => {
+          if (reward !== 0) {
+            return (
+              <h5>Platz {i+1}</h5>
+            )
+          }
+        })}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(store) {
+  return {
+    riders : store.riders.riders
+  }
+}
+
+export default connect(mapStateToProps)(SingleJudgment);
