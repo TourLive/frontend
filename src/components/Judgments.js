0f8553d2a131198b01fa03c80a55c86a8a7f01d7
Urs Forrer <mail@ursforrer.ch>
2018-04-23 10:39:03 +0200

    Done judgement screen for now
    Done judgement screen for now

--- src/components/Judgments.js
+++ src/components/Judgments.js
@@ -1,9 +1,10 @@
 import React, {Component} from "react";
-import {Header} from "semantic-ui-react";
+import {Header, Button, Container, Divider} from "semantic-ui-react";
 import {Helmet} from "react-helmet";
 import { connect } from 'react-redux'
 import store from "../store"
 import * as judgmentRiderConnectionActions from '../actions/judgmentRiderConnectionActions';
+import {Timeline, TimelineEvent} from 'react-event-timeline';
 
 class Judgments extends Component {
     constructor(props){
@@ -11,17 +12,39 @@ class Judgments extends Component {
 
       this.state = {
         updated: false,
-        selected : false
-      }
+        selected : false,
+        judgmentSelected: false
+      };
+
+      this.onJudgmentClicked = this.onJudgmentClicked.bind(this);
+      this.onJudgmentClose = this.onJudgmentClose.bind(this);
     }
 
     fetchJudgmentRiderConnections(id) {
         store.dispatch(judgmentRiderConnectionActions.getJudgmentRiderConnections(id));
     }
 
+    onJudgmentClicked(element) {
+        console.log("IM HERE");
+        console.log(element);
+        this.setState({judgmentSelected : true});
+    }
+
+    onJudgmentClose() {
+
+    }
+
     render() {
         const {actualStage} = this.props;
         const {judgments} = this.props;
+
+        const divStyle = {
+            'box-shadow': 'none'
+        };
+
+        const iconStyle = {
+            'background-color': '#6fba1c'
+        };
 
         if (actualStage.id !== undefined && !this.state.updated) {
           this.fetchJudgmentRiderConnections(actualStage.id);
@@ -33,23 +56,24 @@ class Judgments extends Component {
                     <title>Wertungen</title>
                 </Helmet>
                 <Header as="h1" color='red'>Wertungen</Header>
-                <h1>Wertungen</h1>
-              {judgments.sort((a, b) => a.distance > b.distance).map(judgment => {
-                console.log(judgment);
-                return (
-                  <div key={judgment.id}>
-                    <h3>{judgment.name}</h3>
-                    <span>{judgment.distance}</span>
-                    {judgment.reward.points.map((reward,i) => {
-                      if (reward !== 0) {
+                <Timeline className="App-Timeline">
+                    {judgments.sort((a, b) => a.distance < b.distance).map(judgment => {
+                        const marker = "KM: " + judgment.distance + " | " + judgment.name;
                         return (
-                          <h1>Platz {i+1}</h1>
+                            <TimelineEvent contentStyle={divStyle} bubbleStyle={iconStyle} createdAt={marker}>
+                                <Button onClick={this.onJudgmentClicked}>Infos zur Wertung</Button>
+                            </TimelineEvent>
                         )
-                      }
                     })}
-                  </div>
-              )
-              })}
+                </Timeline>
+                {this.state.judgmentSelected === true &&
+                    <div>
+                        <Divider />
+                        <Container>
+                            <h1>ON JUDGMENT CLICKED</h1>
+                        </Container>
+                    </div>
+                }
             </div>
         );
     }
