28cd720e58313c571a3d5df004f874ca5bd76fb5
Hawner <dominik_good@hotmail.com>
2018-04-30 09:00:26 +0200

    working

--- src/components/SingleTrikotActual.js
+++ src/components/SingleTrikotActual.js
@@ -1,35 +1,57 @@
 import React, { Component } from 'react'
 import {List, Image,Container} from "semantic-ui-react";
 import { connect } from 'react-redux'
+import store from "../store";
+import * as riderStageConnectionActions from "../actions/riderStageConnectionsActions";
 
 class SingleTrikotActual extends Component {
 
-  render() {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            updated: false
+        }
+    }
+
+    fetchRiderStageConnections(id) {
+        store.dispatch(riderStageConnectionActions.getRiderStageConnectionsFromAPI(id));
+        this.setState({updated: true});
+    }
+
+    render() {
         const trikot = this.props.data;
+        const {actualStage} = this.props;
+
+        if (actualStage.id !== undefined && !this.state.updated) {
+            this.fetchRiderStageConnections(actualStage.id);
+        }
+
         const {cons} = this.props;
-        const {maillots} = this.props;
 
         const leader = cons.sort((a,b) => a.virtualGap - b.virtualGap)[0].rider;
         const mountain = cons.sort((a,b) => a.virtualGap - b.virtualGap)[0].rider;
         const point = cons.sort((a,b) => a.virtualGap - b.virtualGap)[0].rider;
         const bestSwiss = cons.filter(con => con.rider.country === 'SUI').sort((a,b) => a.virtualGap - b.virtualGap)[0].rider;
+        
 
-
-      const attachedRider = trikot.type !== 'leader' ? (
-          <p>Fahrer wird geladen</p>
-      ) : (
-          <p>{leader.startNr}, {leader.country}, {leader.name}, {leader.teamName}, Rang</p>
-      );
-
-      const test = trikot.type === 'leader' ? (
+      const attachedRider = trikot.type === 'leader' ? (
           <p>{leader.startNr}, {leader.country}, {leader.name}, {leader.teamName}, Rang</p>
       ) : (
           trikot.type === 'mountain' ? (
               <p>{mountain.startNr}, {mountain.country}, {mountain.name}, {mountain.teamName}, Rang</p>
           ) : (
-              <p>{mountain.startNr}, {mountain.country}, {mountain.name}, {mountain.teamName}, Rang</p>
+              trikot.type === 'points' ? (
+                  <p>{point.startNr}, {point.country}, {point.name}, {point.teamName}, Rang</p>
+              ):
+                  trikot.type === 'bestSwiss' ? (
+                          <p>{bestSwiss.startNr}, {bestSwiss.country}, {bestSwiss.name}, {bestSwiss.teamName}, Rang</p>
+                  ) : (
+                        <p>Fahrerdaten werden geladen</p>
+                      )
           )
-      );
+      )
+
 
     return(
         <List.Item>
@@ -38,8 +60,8 @@ class SingleTrikotActual extends Component {
                 <div className="App-Trikot-Box">
                     <List.Header>{trikot.name}</List.Header>
                     <Container className="App-Trikot-Content">
-                        <span><b>Partner:</b> {trikot.partner}{trikot.type}<br/></span>
-                        {test}
+                        <span><b>Partner:</b> {trikot.partner}<br/></span>
+                        {attachedRider}
                     </Container>
                 </div>
             </List.Content>
@@ -50,6 +72,7 @@ class SingleTrikotActual extends Component {
 
 function mapStateToProps(store) {
   return {
+      actualStage : store.actualStage.data,
       cons : store.cons.cons,
       maillots : store.maillots.data
   }
