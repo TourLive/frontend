e7b45d3dc658bb8e77bc8473fe5efd7dce6767c9
Hawner <dgood@hsr.ch>
2018-05-15 09:59:46 +0200

    Removed unused things
    Removed unused things

--- src/components/views/HeightView.js
+++ src/components/views/HeightView.js
@@ -6,26 +6,17 @@ import {Line} from 'react-chartjs-2';
 class HeightView extends Component {
 
     distance(elementOne, elementTwo) {
-        // Get numeric values out of form elements.
+        // Source: http://www.guymon.de/wordpress/2010/01/25/entfernung-zwischen-zwei-latlong-werten-berechnen/
         var lat_1 = elementOne.latitude;
         var lat_2 = elementTwo.latitude;
         var lon_1 = elementOne.longitude;
         var lon_2 = elementTwo.longitude;
-        // Compute spherical coordinates
-        var rho = 3960.0; // earth diameter in miles
-        // convert latitude and longitude to spherical coordinates in radians
-        // phi = 90 - latitude
+        var rho = 3960.0;
         var phi_1 = (90.0 - lat_1)*Math.PI/180.0;
         var phi_2 = (90.0 - lat_2)*Math.PI/180.0;
-        // theta = longitude
         var theta_1 = lon_1*Math.PI/180.0;
         var theta_2 = lon_2*Math.PI/180.0;
-        // compute spherical distance from spherical coordinates
-        // arc length = \arccos(\sin\phi\sin\phi'\cos(\theta-\theta') + \cos\phi\cos\phi')
-        // distance = rho times arc length
         var d = rho*Math.acos( Math.sin(phi_1)*Math.sin(phi_2)*Math.cos(theta_1 - theta_2) + Math.cos(phi_1)*Math.cos(phi_2) );
-        // Display result in miles and in kilometers
-        var output = "Distance = " + d + " miles or " + 1.609344*d + " kilometers";
         return 1.609344*d;
     }
 
@@ -37,7 +28,6 @@ class HeightView extends Component {
         let lastElement  = null;
         gpsData.map(element => {
             let distance = 0.0;
-            // If first element
             if (lastElement === null) {
                 distance = 0.0;
             } else {
