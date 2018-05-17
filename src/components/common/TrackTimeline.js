08762259d30ce251445d091ddfde810976eb0789
Hawner <dominik_good@hotmail.com>
2018-05-17 21:33:22 +0200

    Revert "react key fix"
    This reverts commit c5743937ebc97534e9034e197f3d8299008dc85b.

--- src/components/common/TrackTimeline.js
+++ src/components/common/TrackTimeline.js
@@ -7,11 +7,11 @@ class TrackTimeline extends Component {
         const elements = this.props.elements;
         let bottomSize = 0;
         return(
-            <section className="timeline">
+            <section class="timeline">
                 <ul className="App-Timeline">
                     {elements.map((element,i) => {
                         if(elements.length === i + 1){bottomSize = element.distance;}
-                        return (<TrackTimelineElement key={i} data={element}/>);
+                        return (<TrackTimelineElement data={element}/>);
                     })}
                     <TrackTimelineEndElement size={bottomSize}/>
                 </ul>
