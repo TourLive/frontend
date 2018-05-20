a956a8b02e582014ff178a8cf778eae81b848a02
Hawner <dominik_good@hotmail.com>
2018-05-20 20:08:06 +0200

    Rider Race Group Container

---
+++ src/containers/RiderRaceGroupContainer.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import RiderRaceGroup from "../components/RiderDetail/RiderRaceGroup";
+
+function mapStateToProps(store) {
+  return {
+    actualStage : store.actualStage.data,
+    logs : store.logsRaceGroupHistory.data
+  }
+}
+
+const RiderRaceGroupContainer = connect(mapStateToProps)(RiderRaceGroup)
+
+export default RiderRaceGroupContainer;
