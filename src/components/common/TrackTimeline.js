edb4f3fc9b6d53557d596fee77936b11a6109951
Urs Forrer <mail@ursforrer.ch>
2018-06-07 13:01:32 +0200

    Done some fixes
    Done some fixes

--- src/components/common/TrackTimeline.js
+++ src/components/common/TrackTimeline.js
@@ -7,16 +7,11 @@ class TrackTimeline extends Component {
     constructor(props) {
         super(props);
         this.ref = null;
-
-        this.setRef = element => {
-            this.ref = element;
-        }
     }
     componentDidUpdate() {
         if (this.ref !== null) {
             scrollToComponent(this.ref);
         }
-        //this.ref.focus();
     }
 
     render() {
@@ -26,7 +21,6 @@ class TrackTimeline extends Component {
             <section className="timeline">
                 <ul className="App-Timeline">
                     {elements.map((element,i) => {
-                        console.log(element);
                         if(elements.length === i + 1){bottomSize = element.distance;}
                         if (element.text === "FELD") {
                             return (<TrackTimelineElement key={i} ref={e => this.ref = e } data={element}/>);
