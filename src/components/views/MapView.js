1fc11df41241011469b85b99cee287ac5ee69772
Urs Forrer <mail@ursforrer.ch>
2018-06-08 12:46:24 +0200

    Done some other work
    Done some other work

--- src/components/views/MapView.js
+++ src/components/views/MapView.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import {Header} from "semantic-ui-react";
 import {Helmet} from "react-helmet";
 import {Map, TileLayer, Marker, Popup, Polyline, Tooltip} from "react-leaflet";
 import Leaflet from "leaflet";
