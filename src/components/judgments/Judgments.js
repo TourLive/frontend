0b430618573cb9354d4e6f86c817f7aa3e63966d
Urs Forrer <mail@ursforrer.ch>
2018-06-11 11:25:41 +0200

    Refactoring of the judgments
    Refactoring of the judgments

--- src/components/judgments/Judgments.js
+++ src/components/judgments/Judgments.js
@@ -1,11 +1,11 @@
 import React, {Component} from "react";
-import {Header, Button} from "semantic-ui-react";
+import {Header} from "semantic-ui-react";
 import {Helmet} from "react-helmet";
 import store from "../../store"
 import * as judgmentRiderConnectionActions from '../../actions/judgmentRiderConnectionActions';
 import SingleJudgmentContainer from "../../containers/judgments/SingleJudgmentContainer";
-import {Timeline, TimelineEvent} from 'react-event-timeline';
 import TimeLineEndBlock from "../common/TimeLineEndBlock";
+import JudgmentTimeline from "./timeline/JudgmentTimeline";
 
 class Judgments extends Component {
     constructor(props){
@@ -14,13 +14,8 @@ class Judgments extends Component {
       this.state = {
         updated: false,
         selected : false,
-        judgmentSelected: false,
-        judgment : {},
         timer : null
       };
-
-      this.onJudgmentClicked = this.onJudgmentClicked.bind(this);
-      this.onJudgmentClose = this.onJudgmentClose.bind(this);
     }
 
     componentDidMount() {
@@ -44,61 +39,28 @@ class Judgments extends Component {
         this.setState({updated : true});
     }
 
-    onJudgmentClicked(element) {
-        this.setState({judgmentSelected : true});
-        this.setState({judgment : element});
-    }
-
-    onJudgmentClose() {
-        this.setState({judgmentSelected : false});
-        this.setState({judgment : {}});
-    }
-
     render() {
         const {actualStage} = this.props;
         const {judgments} = this.props;
 
-        const divStyle = {
-            boxShadow: 'none'
-        };
-
-        const iconStyle = {
-            backgroundColor: '#6fba1c'
-        };
-
         if (actualStage.id !== undefined && !this.state.updated) {
           this.fetchInitalJudgmentRiderConnections(actualStage.id);
         }
-
-        const halfHeight = this.state.judgmentSelected === true ? (
-            "App-Timeline half"
-        ) : (
-            "App-Timeline full"
-        );
 
         return(
             <div className="App-Content">
                 <Helmet>
                     <title>Wertungen</title>
                 </Helmet>
-                {this.state.judgmentSelected === false &&
+                {judgments.display === false &&
                   <div>
                     <Header as="h1" color='red'>Wertungen</Header>
                     <TimeLineEndBlock content="ZIEL"/>
-                    <Timeline className={halfHeight}>
-                      {judgments.sort((a, b) => b.distance - a.distance).map(judgment => {
-                        const marker = "KM: " + judgment.distance + " | " + judgment.name;
-                        return (
-                          <TimelineEvent key={judgment.id} title='' contentStyle={divStyle} bubbleStyle={iconStyle} createdAt={marker}>
-                            <Button value={judgment.id} onClick={()=>this.onJudgmentClicked(judgment)}>Infos zur Wertung</Button>
-                          </TimelineEvent>
-                        )
-                      })}
-                    </Timeline>
+                    <JudgmentTimeline elements={judgments.data}/>
                     <TimeLineEndBlock content="START"/>
                   </div>
                 }
-                {this.state.judgmentSelected === true &&
+                {judgments.display === true &&
                     <SingleJudgmentContainer data={this.state.judgment} close={this.onJudgmentClose}/>
                 }
             </div>
